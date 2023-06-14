c6014729e4d2b8ba7290f285850b238919d72c0e
shenqing <shenqin1@msu.edu>
2023-06-14 12:48:14 -0400

    Initial Commit

---
+++ APIFootball/APIFootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIFootballApp.swift
+//  APIFootball
+//
+//  Created by 沈清昊 on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct APIFootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
